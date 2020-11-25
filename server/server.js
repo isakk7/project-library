ec793759d9f61dfeb727c966dd97bb44404bb9c0
isakk7 <blueisaac@live.com>
2020-11-25 14:25:14 -0600

    Creacion de una Carpeta de rutas

--- server/server.js
+++ server/server.js
@@ -5,73 +5,26 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
+
 app.use(bodyParser.urlencoded({ extended:false}));
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('<h1>Bienvenido a mi servidor Rest (localhost)</h1>');
-});
-  
-app.get('/usuario', function (req, res) {
-  res.json({
-    ok: 200,
-    mensaje: 'Usuario insertado con exito'
-  });
-});
-   
-
-  app.post('/usuario', function (reg, res){
-    let nombre = req.body.nombre;
-    let body = req.body;
-
-    if(nombre === undefined){
-      res.status(400).json({
-      ok: 400,
-      mensaje: 'Favor de enviar el valor del nombre'
-      });
-    }else{
-
-      res.json({
-       ok: 200,
-       mensaje: 'Usuario insertado con exito',
-       body: body
-       
-      });
-    }
+app.get('/', function(req, res) {
+  res.send('<h1>Bienvenido a mi servidor REST</h1>');
 });
 
-app.put('/usuario/:id/:nombre', function(req, res) {
-let id = req.params.id;
-let nombre = req.params.nombre;
+app.set(require('./routes/usuario.js')); 
 
-res.json({
-  ok: 200,
-  mensaje: 'Usuario actualixado con exito',
-  id: id,
-  nombre: nombre
-});
-});
-app.delete('/usuario/:id', function(req, res) {
-let id = req.params.id;
-
-res.json({
-  ok: 200,
-  mensaje: 'Usuario eliminado con exito',
-  id: id
-});
-});
 
 mongoose.connect('mongodb://localhost:27017/cafeteria', {
- 
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true
 
-
 }, (err, res) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log('Base de Datos Online');
 
 });
@@ -79,3 +32,4 @@ mongoose.connect('mongodb://localhost:27017/cafeteria', {
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta en linea por el puerto', process.env.PORT);
 });
+
