910cbc00db555bcf4f255b4516d36d9aa37bbfb0
isakk7 <blueisaac@live.com>
2020-11-27 11:17:18 -0600

    Realizada la ruta de usuarios con la funcion POST con BD

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,62 +1,52 @@
 const express = require('express');
-const Usuario = require('../models/usuario');
 const app = express();
 
-
-  app.get('/usuario', function (req, res) {
+app.get('/usuario', function(req, res) {
     res.json({
-      ok: 200,
-      mensaje: 'Usuario insertado con exito'
+        ok: 200,
+        mensaje: 'Usuarios consultados con exito'
     });
-  });
-     
-  
-    app.post('/usuario', function (req, res){
+});
 
-      let body = req.body;
-      let usr = new Usuario({
-        nombre: body.nombre,
-        email: body.email,
-        password: body.password
-      });
+app.post('/usuario', function(req, res) {
+    let nombre = req.body.nombre;
+    let body = req.body;
 
-      usr.save((err, usrDB) => {
-        if(err) {
-          return res.status(400).json({
-            ok: false,
-            msg: 'Ocurrio un error',
-            err
-          });
-        }
+    if (nombre === undefined) {
+        res.status(400).json({
+            ok: 400,
+            mensaje: 'Favor de enviar el valor del nombre'
+        });
+    } else {
 
         res.json({
-          ok:true, 
-          msg: 'Usuario insertado con exito',
-          usrDB
+            ok: 200,
+            mensaje: 'Usuario insertado con exito',
+            body: body
         });
-      });
-   
-  });
-  
-  app.put('/usuario/:id/:nombre', function(req, res) {
-  let id = req.params.id;
-  let nombre = req.params.nombre;
-  
-  res.json({
-    ok: 200,
-    mensaje: 'Usuario actualixado con exito',
-    id: id,
-    nombre: nombre
-  });
-  });
-  app.delete('/usuario/:id', function(req, res) {
-  let id = req.params.id;
-  
-  res.json({
-    ok: 200,
-    mensaje: 'Usuario eliminado con exito',
-    id: id
-  });
-  });
+    }
+});
 
-  module.export = app;
+app.put('/usuario/:id/:nombre', function(req, res) {
+    let id = req.params.id;
+    let nombre = req.params.nombre;
+
+    res.json({
+        ok: 200,
+        mensaje: 'Usuario actualizado con exito',
+        id: id,
+        nombre: nombre
+    });
+});
+
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    res.json({
+        ok: 200,
+        mensaje: 'Usuario eliminado con exito',
+        id: id
+    });
+});
+
+module.exports = app;
